fdd113574a59e106aa4c936e1fa8f20c9f6278d6
alexandertataurov <alex.tataurov@me.com>
2025-08-10 11:37:02 +0400

    chore: add Storybook accessibility addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-a11y"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
